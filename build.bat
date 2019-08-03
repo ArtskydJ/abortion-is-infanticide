d12fa5a47509918441ba92f57156e3284cbbee12
Joseph Dykstra <josephdykstra@gmail.com>
2019-08-03 15:59:24 -0500

    Open ff and chrome extension sites upon building

--- build.bat
+++ build.bat
@@ -3,3 +3,6 @@ del dist.zip
 pushd .\src
 "C:\Program Files\7-Zip\7z.exe" a ..\dist.zip
 popd
+echo "Works on Firefox and Firefox for Android"
+explorer "https://addons.mozilla.org/en-US/developers/addon/abortion-is-infanticide/versions/submit/"
+explorer "https://chrome.google.com/webstore/developer/dashboard"
